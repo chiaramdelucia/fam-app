ab518137a6040dfcef341ecbc196d25b8c950ca5
Chiara DeLucia <chiaradelucia@Chiaras-MacBook-Pro.local>
2017-01-21 13:22:34 -0500

    Scrolling effects, weather widget, and landing page background.

--- assets/javascript/famapp.js
+++ assets/javascript/famapp.js
@@ -1,11 +1,51 @@
-$(".btn1").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#content").offset().top},
-        'slow');
-});
+$(document).ready(function() {
 
-$(".scrollTop").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#land").offset().top},
-        'slow');
+
+    function getWeather() {
+        var address = $("#address").val();
+        console.log("weather " + address)
+        $.ajax({
+
+            url: 'https://api.wunderground.com/api/7d4c2ccc48b6acd9/conditions/q/' + address + '.json',
+            method: 'GET',
+            datatype: "json"
+        }).done(function(wonder) {
+            console.log(wonder)
+            console.log(wonder.current_observation.icon)
+            var icon_url = wonder.current_observation.icon_url
+            var icon = wonder.current_observation.icon
+            var degrees = Math.floor(wonder.current_observation.temp_f)
+            var city = wonder.current_observation.display_location.city
+            $(".weather-widget").html('<span><img src="' + icon_url + '"></span><span>' + degrees + '°F</span><br><span>' + city + '<span>')
+
+        });
+
+    };
+
+    $("#add-address").on("click", function() {
+        event.preventDefault();
+        $('html,body').animate({
+       		scrollTop: $("#content").offset().top},
+        	'slow');
+ 
+        getWeather();
+
+    });
+
+    var win = $(window),
+            nav = $('nav'),
+
+            pos = nav.offset().top,
+            sticky = function() {
+                win.scrollTop() > pos ?
+                    nav.addClass('sticky')
+                    : nav.removeClass('sticky')
+            }
+
+    win.scroll(sticky)
+
+   
+
+
+
 });
