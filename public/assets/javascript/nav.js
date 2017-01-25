d184902d77e5a63246e546033a33f8dc428ea05e
Asha ST <ashast@Ashas-MacBook-Pro.local>
2017-01-25 16:57:34 -0500

    drop down for results,n temporary uniformity of font

--- public/assets/javascript/nav.js
+++ public/assets/javascript/nav.js
@@ -40,12 +40,17 @@ $(document).ready(function(){
 		event.preventDefault();
 		localStorage.setItem("cityState", cityState);
 		$("#resultsAPI").empty();
+    getWeather();
 		whatTheYelp();	  
 	});
 
 
   function whatTheYelp(){
     var address = $("#google-input-zip").val();
+
+    var numResults = $("#numResults").val();
+    console.log("number of results to fetch", numResults);
+
       
       console.log("whatTheYelp() address ", address);
       
@@ -132,7 +137,7 @@ $(document).ready(function(){
           //start "here"
           initMap(initlat, initlong);
 
-		for(i=0; i<=9; i= i+1) {
+		for(i=0; i< numResults; i= i+1) {
 			//commenting this out because of bookmarking error
 
 			// $("#resultsAPI").append("<p class='link'>");  
@@ -186,6 +191,9 @@ function getMeetups(){
     
     console.log('getMeetups() - zipcode', localStorage.getItem("userZip"));
     userZip = localStorage.getItem("userZip");
+    var numResults = $("#numResults").val();
+
+    console.log("number of results to fetch", numResults);
 
     // var meetupsAPIKey = "32246d5033476b30277fe2c671b1b";
    
@@ -200,7 +208,7 @@ function getMeetups(){
       }).done(function(response) {
         console.log("getMeetups() ", response);
 
-        for(var i=0; i< 10; i++){
+        for(var i=0; i< numResults; i++){
 
           var outPutDivSection = $('<div>');
           outPutDivSection.attr("class", "search-result");
